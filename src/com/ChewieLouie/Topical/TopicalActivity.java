8b1830dc79c9d394501fdfd9f645b191f539d2d6
julianchurchill <chewielouie03@gmail.com>
2011-09-11 19:38:03 +0100

    Added topics list front page

--- src/com/ChewieLouie/Topical/TopicalActivity.java
+++ src/com/ChewieLouie/Topical/TopicalActivity.java
@@ -4,14 +4,18 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 
 public class TopicalActivity extends Activity {
-
+	String[] topics = { "Topic 1", "Topic 2", "Topic 3" };
+	
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        populateTopicList();
     }
 
     public void search( View view ) {
@@ -20,5 +24,12 @@ public class TopicalActivity extends Activity {
     	intent.putExtra( TopicalConstants.IntentExtraKey_Conversations,
     			GooglePlusIfc.search( editText.getText().toString() ) );
     	startActivity( intent );
+    }
+    
+    private void populateTopicList()
+    {
+    	ListView conversationList = (ListView)findViewById( R.id.topicList );
+    	conversationList.setAdapter( new ArrayAdapter<String>( this, R.layout.conversation_list_item, 
+        		R.id.conversation_list_item_text, topics ) );
     }
 }
