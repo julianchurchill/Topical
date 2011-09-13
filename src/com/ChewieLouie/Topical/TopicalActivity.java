7c9291f9d9c23aee126226357296fdef6a969555
julianchurchill <chewielouie03@gmail.com>
2011-09-13 22:06:21 +0100

    Added click handling for topic list

--- src/com/ChewieLouie/Topical/TopicalActivity.java
+++ src/com/ChewieLouie/Topical/TopicalActivity.java
@@ -4,34 +4,59 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.AdapterView.OnItemClickListener;
 
 public class TopicalActivity extends Activity {
-	String[] topics = { "Topic 1", "Topic 2", "Topic 3" };
 	
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         populateTopicList();
+        addTopicItemClickNotifier();
     }
 
     public void search( View view ) {
     	EditText editText = (EditText)findViewById( R.id.SearchText );
+    	String topic = editText.getText().toString();
+    	startActivity( createTopicListIntent( topic, GooglePlusIfc.search( topic ) ) );
+    }
+    
+    private Intent createTopicListIntent( String topic, String[] listContents )
+    {
     	Intent intent = new Intent().setClass( getApplicationContext(), TopicListActivity.class );
-    	intent.putExtra( TopicalConstants.IntentExtraKey_ConversationsSearchString,
-    			editText.getText().toString() );
-    	intent.putExtra( TopicalConstants.IntentExtraKey_Conversations,
-    			GooglePlusIfc.search( editText.getText().toString() ) );
-    	startActivity( intent );
+    	intent.putExtra( TopicalConstants.IntentExtraKey_ConversationsSearchString, topic );
+    	intent.putExtra( TopicalConstants.IntentExtraKey_Conversations, listContents );
+    	return intent;
     }
     
     private void populateTopicList()
     {
+    	final String[] topics = { "Topic 1", "Topic 2", "Topic 3" };
     	ListView conversationList = (ListView)findViewById( R.id.topicList );
     	conversationList.setAdapter( new ArrayAdapter<String>( this, R.layout.conversation_list_item, 
         		R.id.conversation_list_item_text, topics ) );
+    }
+
+    private void addTopicItemClickNotifier()
+	{
+	    ListView lv = (ListView)findViewById( R.id.topicList );
+	    lv.setOnItemClickListener( new OnItemClickListener() {
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+			{
+				onTopicListClicked( view, position );
+			}
+		});
+	}
+    
+    protected void onTopicListClicked( View view, int position )
+    {
+    	final String topic = "test topic";
+    	final String[] listContents = { "test contents1", "test contents2", "test contents3" };
+    	startActivity( createTopicListIntent( topic, listContents ) );
     }
 }
